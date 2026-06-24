eaf6a5602bf2ca80b22e1ccc95cb572ecc7dd21d
minhtamhtyb <minhtamhtyb@gmail.com>
2026-06-24 21:41:06 +0700

    first commit

---
+++ SportCourtManagement_FrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SportCourtManagement_FrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
